31b9c9d89c4f4ab9781100c4522b20a189c6673e
Daniel Cheang <daniel@somewhere>
2022-01-14 02:26:08 +0800

    use sendFile

--- server.js
+++ server.js
@@ -13,124 +13,112 @@ app.use(express.static(BASE_PATH));
 const jsonParser = bodyParser.json();
 app.use(jsonParser);
 
-
-app.get("/", function(req, res){
-  res.send("Hello World!");
+app.get("/", function (req, res) {
+	res.send("Hello World!");
 });
 
-
-function createDir(dir){
-  const dirPath = path.join(BASE_PATH, dir);
-  if (!fs.existsSync(dirPath)) {
-    fs.mkdirSync(dirPath);
-  }
+function createDir(dir) {
+	const dirPath = path.join(BASE_PATH, dir);
+	if (!fs.existsSync(dirPath)) {
+		fs.mkdirSync(dirPath);
+	}
 }
-    
+
 function randomItem(array) {
-  const idx = Math.floor(Math.random() * array.length);
-  return array[idx];
+	const idx = Math.floor(Math.random() * array.length);
+	return array[idx];
 }
 
 function randomKey(keyLen = 7) {
-  const DOWN_CASE_LETTERS = "abcdefghijklmnopqrstuvwxyz".split("");
-  const UP_CASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-  const NUMBERS = "0123456789".split("");
-  const CHARS = [...DOWN_CASE_LETTERS, ...UP_CASE_LETTERS, ...NUMBERS];
-  const len = CHARS.length;
+	const DOWN_CASE_LETTERS = "abcdefghijklmnopqrstuvwxyz".split("");
+	const UP_CASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+	const NUMBERS = "0123456789".split("");
+	const CHARS = [...DOWN_CASE_LETTERS, ...UP_CASE_LETTERS, ...NUMBERS];
+	const len = CHARS.length;
 
-  return [
-    randomItem(DOWN_CASE_LETTERS),
-    ...new Array(keyLen - 1).fill().map(() => randomItem(CHARS)),
-  ].join("");
+	return [
+		randomItem(DOWN_CASE_LETTERS),
+		...new Array(keyLen - 1).fill().map(() => randomItem(CHARS)),
+	].join("");
 }
 
-function getFilePath(req){
-  const dir = req.params.resources;
-  const id = req.params.id;
-  const basename = path.basename(id, ".json");
+function getFilePath(req) {
+	const dir = req.params.resources;
+	const id = req.params.id;
+	const basename = path.basename(id, ".json");
 	return path.join(BASE_PATH, dir, `${basename}.json`);
 }
 
-app.get("/api/:resources", function(req, res){
-  console.log(req.params);
-  const dir = req.params.resources;
-  const dirPath = path.join(BASE_PATH, dir);
+app.get("/api/:resources", function (req, res) {
+	console.log(req.params);
+	const dir = req.params.resources;
+	const dirPath = path.join(BASE_PATH, dir);
 
-  if (!fs.existsSync(dirPath)) {
-    res.sendStatus(404);
-    return;
-  }
+	if (!fs.existsSync(dirPath)) {
+		res.sendStatus(404);
+		return;
+	}
 
-  const files = fs.readdirSync(dirPath).map((file) => {
-    return path.basename(file, ".json");
-  });
-  res.json(files);
+	const files = fs.readdirSync(dirPath).map((file) => {
+		return path.basename(file, ".json");
+	});
+	res.json(files);
 });
 
-app.post("/api/:resources", function(req, res){
-  const dir = req.params.resources;
-  createDir(dir);
+app.post("/api/:resources", function (req, res) {
+	const dir = req.params.resources;
+	createDir(dir);
 
-  const key = randomKey();
+	const key = randomKey();
 	const filePath = path.join(BASE_PATH, dir, `${key}.json`);
-  const publicPath = path.join("/", dir, `${key}.json`);
+	const publicPath = path.join("/", dir, `${key}.json`);
 
 	const json = JSON.stringify(req.body);
-	fs.writeFile(filePath, json, "utf8", function(){
-		res.send({id: key, path: publicPath});
+	fs.writeFile(filePath, json, "utf8", function () {
+		res.send({ id: key, path: publicPath });
 	});
 });
 
+app.get("/api/:resources/:id", function (req, res) {
+	const filePath = getFilePath(req);
 
-app.get("/api/:resources/:id", function(req, res){
-  const filePath = getFilePath(req);
+	if (!fs.existsSync(filePath)) {
+		res.sendStatus(404);
+		return;
+	}
 
-  if (!fs.existsSync(filePath)) {
-    res.sendStatus(404);
-    return;
-  }
-
-  // TODO: serve the file directly
-  fs.readFile(filePath, (err, json) => {
-    const obj = JSON.parse(json);
-    res.json(obj);
-  });
+	res.sendFile(filePath, { root: __dirname });
 });
 
-app.put("/api/:resources/:id", function(req, res){
-  const filePath = getFilePath(req);
+app.put("/api/:resources/:id", function (req, res) {
+	const filePath = getFilePath(req);
 
-  if (!fs.existsSync(filePath)) {
-    res.sendStatus(404);
-    return;
-  }
+	if (!fs.existsSync(filePath)) {
+		res.sendStatus(404);
+		return;
+	}
 
 	const json = JSON.stringify(req.body);
-	fs.writeFile(filePath, json, "utf8", function(){
-    fs.readFile(filePath, (err, json) => {
-      const obj = JSON.parse(json);
-      res.json(obj);
-    });
+	fs.writeFile(filePath, json, "utf8", function () {
+		res.sendFile(filePath, { root: __dirname });
 	});
 });
 
-app.delete("/api/:resources/:id", function(req, res){
-  const filePath = getFilePath(req);
-  console.log(filePath);
+app.delete("/api/:resources/:id", function (req, res) {
+	const filePath = getFilePath(req);
+	console.log(filePath);
 
-  if (!fs.existsSync(filePath)) {
-    res.sendStatus(404);
-    return;
-  }
+	if (!fs.existsSync(filePath)) {
+		res.sendStatus(404);
+		return;
+	}
 
-  fs.unlinkSync(filePath);
-  res.json({msg: "resource deleted"});
+	fs.unlinkSync(filePath);
+	res.json({ msg: "resource deleted" });
 });
 
+app.listen(PORT, function () {
+	const { address, port } = server.address();
 
-const server = app.listen(PORT, function(){
-  const { address, port } = server.address();
-
-  console.log("url: http://%s:%s", address, port);
+	console.log("url: http://%s:%s", address, port);
 });
-
